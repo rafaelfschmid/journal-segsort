cbd43391a31373eba105962e5254762c6382ad47
Rafael Schmid <rafaelfschmid@gmail.com>
2020-05-06 15:51:25 -0400

    nthrust exec

--- scripts/genexecnthrust.sh
+++ scripts/genexecnthrust.sh
@@ -2,10 +2,7 @@
 out=$1
 generator=$2
 
-echo "" > $out/newthrustthr.time
-echo "" > $out/newthrustschedthr.time
-echo "" > $out/nthrust.time			
-	
+echo "" > $out/nthrust.time		
 
 n=32768
 while [ $n -le 134217728 ]
@@ -13,20 +10,14 @@ do
 	s=1
 	while [[ $s -lt $n && $s -le 1048576 ]] 
 	do
-		echo -e "\n"$s"\n"$n >> $out/newthrustthr.time
-		echo -e "\n"$s"\n"$n >> $out/newthrustschedthr.time
 		echo -e "\n"$s"\n"$n >> $out/nthrust.time				
-		
 		i=1
 		while [ $i -le  10 ] 
 		do
 			in=$s"_"$n"_"$i".in"
 			./$generator $s $n > $in
 
-			./fixpasscub.exe 	< $in 	>> $out/newthrustthr.time
-			./fixpassthrust.exe	< $in	>> $out/newthrustschedthr.time
-			./fixpassthrust.exe	< $in	>> $out/nthrust.time				
-
+			./nthrust.exe	< $in	>> $out/nthrust.time				
 			rm -f $in
 			((i=$i+1))
 		done
